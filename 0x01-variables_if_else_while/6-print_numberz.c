6cd8f52f7d73dfd0fca514f0493a137f72b25c53
EGNIARNOLD <egniarnold@gmail.com>
2022-03-14 22:14:57 -0700

    My c project

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,12 +9,12 @@ int main(void)
 {
 		int c = 0;
 
-			while (c < 10)
-					{
-								putchar(48 + c);
-										c++;
-											}
+		while (c < 10)
+		{
+			putchar(48 + c);
+			c++;
+		}
 
-				putchar('\n');
-					return (0);
+	        putchar('\n');
+		return (0);
 }
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -11,12 +11,12 @@ int main(void)
 {
 		char c = 'z';
 
-			while (c >= 'a')
-					{
-								putchar(c);
-										c--;
-											}
+	        while (c >= 'a')
+		{
+			putchar(c);
+			c--;
+		}
 
-				putchar('\n');
-					return (0);
+		putchar('\n');
+		return (0);
 }
