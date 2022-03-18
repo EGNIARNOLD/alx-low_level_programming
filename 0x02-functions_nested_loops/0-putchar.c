f177b92f01bd24979661b3ab0c225d0117cc1dbc
EGNIARNOLD <egniarnold@gmail.com>
2022-03-18 15:12:56 -0700

    My c functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
-
-
 /**
- * main - Entry point of the program
- *
- * return: Always return 0 (Success)
+ * main - Entry point
+ * Return:0
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char c[10] = "_putchar";
+	int i = 0;
+
+	while (i < 9)
+	{
+
+		_putchar(c[i]);
+		i++;
+	}
 	_putchar('\n');
 
 	return (0);
