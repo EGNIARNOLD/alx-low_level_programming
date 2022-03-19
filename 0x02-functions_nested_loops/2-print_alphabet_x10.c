97331d66210f2a675e37f0f88b3f554b12afc4b9
EGNIARNOLD <egniarnold@gmail.com>
2022-03-18 19:44:38 -0700

    My c functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,9 @@
 void print_alphabet_x10(void)
 {
 	char c;
-	int i = 0
+	int i = 0;
 
 	while (i < 10)
-
 	{
 		c = 'a';
 		while (c <= 'z')
@@ -20,6 +19,7 @@ void print_alphabet_x10(void)
 			c++;
 		}
 		_putchar('\n');
-		 i++;
+		i++;
 	}
+
 }
